73a487e32f49787d6f54b7eb88acc486346f4456
tasinirmaz <keskinrecep26@gmail.com>
2025-11-26 16:14:06 +0300

    de

--- tour/data.js
+++ tour/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-karakaya",
-      "name": "karakaya",
+      "id": "0-20251126_152852_434",
+      "name": "20251126_152852_434",
       "levels": [
         {
           "tileSize": 256,
@@ -22,32 +22,25 @@ var APP_DATA = {
           "size": 2048
         }
       ],
-      "faceSize": 2048,
+      "faceSize": 2000,
       "initialViewParameters": {
-        "yaw": 0.1821936445106438,
-        "pitch": 0.3556540610571126,
-        "fov": 1.2599180821480807
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": -0.1757219636188605,
-          "pitch": 0.5491282937901971,
+          "yaw": -0.12054563121198747,
+          "pitch": 0.2783591151412228,
           "rotation": 0,
-          "target": "1-kayack"
+          "target": "1-20251126_153135_015"
         }
       ],
-      "infoHotspots": [
-        {
-          "yaw": -0.8193059419904483,
-          "pitch": 0.33285464251583896,
-          "title": "Salon",
-          "text": "6,5M - 4M (26M²)"
-        }
-      ]
+      "infoHotspots": []
     },
     {
-      "id": "1-kayack",
-      "name": "kayacık",
+      "id": "1-20251126_153135_015",
+      "name": "20251126_153135_015",
       "levels": [
         {
           "tileSize": 256,
@@ -67,35 +60,28 @@ var APP_DATA = {
           "size": 2048
         }
       ],
-      "faceSize": 2048,
+      "faceSize": 2000,
       "initialViewParameters": {
-        "yaw": 0.2862463929206278,
-        "pitch": 0.6931549157156507,
-        "fov": 1.2599180821480807
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": 0.2862463929206278,
-          "pitch": 0.6931549157156507,
+          "yaw": 0.28222131214146273,
+          "pitch": 0.23307806979041068,
           "rotation": 0,
-          "target": "0-karakaya"
+          "target": "0-20251126_152852_434"
         }
       ],
-      "infoHotspots": [
-        {
-          "yaw": -0.6493463031220585,
-          "pitch": 0.565619960767636,
-          "title": "Mutfak",
-          "text": "4M - 3M (12M²)"
-        }
-      ]
+      "infoHotspots": []
     }
   ],
-  "name": "bahcelievler_tekniklise_160_1",
+  "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
-    "fullscreenButton": true,
-    "viewControlButtons": true
+    "fullscreenButton": false,
+    "viewControlButtons": false
   }
 };
